7183c26cb0a7572037a1d44287daf55b571aef84
Daci1 <dacian.barbu28@gmail.com>
2025-11-10 00:11:07 +0200

    Adds reset function for avante state

--- modules/home-manager/nvim/config/after/plugin/ai_assist.lua
+++ modules/home-manager/nvim/config/after/plugin/ai_assist.lua
@@ -10,3 +10,18 @@ require('avante').setup({
   provider = "copilot",
   mode = "legacy",
 })
+
+-- User command to reset local state of avante
+-- useful when apply code doesn't work
+local function avante_reset()
+  local path = vim.fn.expand("~/.local/state/nvim/avante")
+  if vim.fn.isdirectory(path) == 1 then
+    vim.fn.delete(path, "rf")  -- "rf" = recursive + force
+    print("Avante directory reset!")
+  else
+    print("Avante directory does not exist.")
+  end
+end
+
+-- Create a custom command that calls this function
+vim.api.nvim_create_user_command("AvanteReset", avante_reset, {})
