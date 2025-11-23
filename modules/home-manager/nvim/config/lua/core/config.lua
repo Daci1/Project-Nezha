c6846f80a589d10fd050543bee2a8b946eabbc4c
Daci1 <dacian.barbu28@gmail.com>
2025-11-23 19:53:52 +0200

    Fixes vertical split not working for some netrw weird reason

--- modules/home-manager/nvim/config/lua/core/config.lua
+++ modules/home-manager/nvim/config/lua/core/config.lua
@@ -18,3 +18,13 @@ vim.opt.undodir = vim.fn.stdpath("cache") .. "/undo"
 
 -- Make search case insensitive
 vim.opt.ignorecase = true
+
+-- netrw-specific mapping override (this is because it overwrites <C-l> by default when splitting)
+local group = vim.api.nvim_create_augroup("FileTypeNetrw", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+  group = group,
+  pattern = "netrw",
+  callback = function ()
+    vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Move to right window', buffer = true, silent = true })
+  end,
+})
