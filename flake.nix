551e11623387dc0763b19e3121035fa0c70096b2
Daci1 <dacian.barbu28@gmail.com>
2025-12-03 17:29:43 +0200

    Updates stable to channel 25.11, updates flake

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    stable.url = "github:nixos/nixpkgs?ref=25.05";
+    stable.url = "github:nixos/nixpkgs?ref=25.11";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
