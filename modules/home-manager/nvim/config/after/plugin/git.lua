3af91d327c8d67a2fbe2c1170c54f698e286c564
Daci1 <dacian.barbu28@gmail.com>
2026-02-05 17:02:11 +0200

    Adds git diff and merge conflict keymaps

--- modules/home-manager/nvim/config/after/plugin/git.lua
+++ modules/home-manager/nvim/config/after/plugin/git.lua
@@ -2,3 +2,14 @@ require("gitsigns").setup({})
 
 vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk_inline<CR>", { desc = "Preview Git Hunk" })
 vim.keymap.set("n", "<leader>gu", ":Gitsigns reset_hunk<CR>", { desc = "Undo Hunk" })
+vim.keymap.set("n", "<leader>gd", ":Gvdiffsplit | wincmd l<CR>", { desc = "Open git diff split" })
+vim.keymap.set("n", "<leader>gc", ":Gvdiffsplit! | wincmd l<CR>", { desc = "Open git diff 3 way split" })
+
+-- DIFF related
+vim.keymap.set("n", "<leader>do", function()
+	vim.cmd("diffoff") -- turn off the diff
+	vim.cmd("wincmd o") -- jump to the previous/other window
+end, { desc = "Close git diff and keep only focused buffer" })
+vim.keymap.set("n", "<leader>dg", ":diffget<CR>", { desc = "Get diff" })
+vim.keymap.set("n", "<leader>dh", ":diffget //2<CR>", { desc = "Merge conflict: get diff from left window" })
+vim.keymap.set("n", "<leader>dl", ":diffget //3<CR>", { desc = "Merge conflict: get diff from right window" })
