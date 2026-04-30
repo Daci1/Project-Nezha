ba9dd7bed88d0a5eff8ade544964c7bbcd9015ec
Daci1 <dacian.barbu28@gmail.com>
2026-04-30 14:36:04 +0300

    adds virtual screen, for sunshine

--- hosts/nixos/hardware-configuration.nix
+++ hosts/nixos/hardware-configuration.nix
@@ -23,7 +23,10 @@
     "sd_mod"
   ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
+  boot.kernelModules = [
+    "kvm-amd"
+    "vkms" # For headless use, so far only for sunshine
+  ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" = {
